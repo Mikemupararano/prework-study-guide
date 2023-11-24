d1f997ba41063515a12e7967a65db0de7c36c715
Mikemupararano <kudath@yahoo.co.uk>
2023-11-24 21:53:40 +0000

    Added conditional logic and functions

--- prework-study-guide/prework-study-guide/assets/script.js
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -1,21 +1,30 @@
-/*var topic = "HTML";
-
-if (topic === 'HTML') {
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function selectTopic() {
+    if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}*/
+}
+}
+
 /*var shapes = ["triangle", "square", "pentagon", "circle"];
 for(var x = 0; x < shapes.length; x++) {
  console.log(shapes[x]);
 }*/
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var i=0; i<topics.length; i++) {
+function listTopics() {
+    for (var i=0; i<topics.length; i++) {
 console.log(topics[i]);
 }
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
