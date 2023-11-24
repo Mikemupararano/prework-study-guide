f99db21bbe92289e2e5b69b732f22679461e6472
Mikemupararano <kudath@yahoo.co.uk>
2023-11-24 20:16:41 +0000

    Started JavaScript variiables and added for loop

---
+++ prework-study-guide/prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+/*var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}*/
+/*var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}*/
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var i=0; i<topics.length; i++) {
+console.log(topics[i]);
+}
